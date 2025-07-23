44ad74102d37c1776f3ad407a951bdfb580b3469
daronjchang <daronjchang@gmail.com>
2025-07-22 22:02:34 -0500

    move to 25.05

--- cfg.nix
+++ cfg.nix
@@ -97,7 +97,7 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05";
   environment.loginShellInit = ''[[ "$(tty)" == /dev/tty1 ]] && dbus-run-session hyprland'';
 
 }
--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
